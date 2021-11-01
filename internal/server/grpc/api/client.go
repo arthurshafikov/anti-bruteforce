12f3798afa103a9eee6ca5f6609083ae721d43ba
thewolf27 <wolf-front@yandex.ru>
2021-11-01 15:24:05 +0300

    add gRPC client

---
+++ internal/server/grpc/api/client.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"context"
+	"log"
+
+	"github.com/thewolf27/anti-bruteforce/internal/server/grpc/generated"
+	"google.golang.org/grpc"
+)
+
+func NewGRPCClient(ctx context.Context, address string) generated.AppServiceClient {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		<-ctx.Done()
+		conn.Close()
+	}()
+
+	return generated.NewAppServiceClient(conn)
+}
